19461b422534b2a9082ecf18f9569df7ef9bd435
zartyblartfast <user@example.com>
2026-02-07 21:17:10 +0000

    Deploy to GitHub Pages

--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,2 @@
 // JS entry to avoid proxy MIME issues. Vite will resolve TS modules.
-import '/src/main.ts';
+import './main.ts';
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/solar-analemma/' : '/',
   plugins: [react()],
   define: {
     'process.env': {},
   },
-});
+}));
